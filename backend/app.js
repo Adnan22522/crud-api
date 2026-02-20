5aa735803993cc99e71b6fd1e606fdcf05e5c6fe
unknown <adnanjunaid18@gmail.com>
2026-02-20 12:17:33 +0500

    Initial commit: CRUD API with secure frontend

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/userRoute.js';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/api-crud')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.use('/api', userRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import User from '../models/userModel.js';
+
+
+const router = express.Router();
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    if(users) return res.json(users);
+  } catch (err) {    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/users', async (req, res) => {
+  try {
+    const users = await User.find({'email': req.body.email});
+    if(users.length > 0) return res.status(400).json({ message: 'User already exists' });
+    const newUser = await User.create(req.body);
+    if(newUser) return res.json(newUser);
+  } catch (err) {    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if(user) return res.json(user);
+  } catch (err) {    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { returnDocument: 'after' });
+    
+    if (!user) return res.status(404).json({ message: "User not found" });
+    
+    // This will now instantly return the freshly updated record!
+    return res.json(user); 
+    
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if(user) return res.json({ message: 'User deleted' });
+  } catch (err) {    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
