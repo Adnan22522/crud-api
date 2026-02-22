0ebe61dbe0097415756dd84424a89ebd8d06823e
unknown <adnanjunaid18@gmail.com>
2026-02-22 12:35:08 +0500

    Added request rate-limiting to protect API from spam

--- backend/app.js
+++ backend/app.js
@@ -3,20 +3,36 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRoute.js';
 import loginRoute from './routes/loginRoute.js';
 import Auth from './middlewares/authMiddleware.js';
+import rateLimit from 'express-rate-limit';
+import slowDown from 'express-slow-down';
 import cors from 'cors';
 const app = express();
 
-app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/api-crud')
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
+const limiter = rateLimit({
+  windowMs: 1000 * 20, // 20 seconds
+  limit: 5, // 5 requests
+  message: 'Too many requests, please try again later.'
+});
+
+const limiter2 = slowDown({
+  windowMs: 1000 * 60,
+  delayAfter: 5,
+  delayMs: (hits) => hits * 1000, // hits is the number of requests made after the limit is reached.
+});
+
+// app.use(limiter);
+app.use(limiter2);
+app.use(cors());
 app.use(express.json());
-
 app.use('/api', loginRoute);
-
 app.use(Auth);
+
+
 app.use('/api', userRouter);
 
 app.listen(3000, () => {
